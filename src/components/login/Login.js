2818e1a90c9b20a4ec2e20a4146f63c8bb798e54
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-04-05 19:09:23 +0200

    1

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,84 +1,91 @@
-import React from "react";
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
 import "./login.css";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+//import HomeSec from '../homepage-sec/pagesec';
 
-export default function login() {
-  return (
-    <Grid component="main" className="root">
-      <Grid className="background">
-        <div className="login-form-container">
-          <img src="/uni_logoo.png" alt="uni_logo" id="logo"></img>
-          <Avatar id="avatar"></Avatar>
-          <Typography
-            component="h1"
-            variant="h4"
-            style={{ fontWeight: "bold" }}
-          >
-            تسجيل الدخول
-          </Typography>
-          <form className="login-form" noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="اسم المستخدم"
-              name="username"
-              autoComplete="username"
-              id="inputLabels"
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="كلمة المرور"
-              type="password"
-              autoComplete="current-password"
-              id="inputLabels"
-            />
-            <div className="form-radio">
-              <div>
-                <label id="radioBtns">
-                  شئون الطلبة
-                  <input
-                    type="radio"
-                    name="selection"
-                    value="student"
-                    style={{ marginLeft: "10px" }}
-                  />
-                </label>
-              </div>
-              <div>
-                <label id="radioBtns">
-                  طالب
-                  <input
-                    type="radio"
-                    name="selection"
-                    value="secretary"
-                    style={{ marginLeft: "10px" }}
-                  />
-                </label>
-              </div>
-            </div>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="lightBlue"
+class Login extends Component {
+  render() {
+    return (
+      <Grid component="main" className="root">
+        <Grid className="background">
+          <div className="login-form-container">
+            <img src="/uni_logoo.png" alt="uni_logo" id="logo"></img>
+            <Avatar id="avatar"></Avatar>
+            <Typography
+              component="h1"
+              variant="h4"
               style={{ fontWeight: "bold" }}
-              id="Btn"
             >
-              الدخول
-            </Button>
-          </form>
-        </div>
+              تسجيل الدخول
+            </Typography>
+            <form className="login-form" noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="اسم المستخدم"
+                name="username"
+                autoComplete="username"
+                id="inputLabels"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="كلمة المرور"
+                type="password"
+                autoComplete="current-password"
+                id="inputLabels"
+              />
+              <div className="form-radio">
+                <div>
+                  <label id="radioBtns">
+                    شئون الطلبة
+                    <input
+                      type="radio"
+                      name="selection"
+                      value="student"
+                      style={{ marginLeft: "10px" }}
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label id="radioBtns">
+                    طالب
+                    <input
+                      type="radio"
+                      name="selection"
+                      value="secretary"
+                      style={{ marginLeft: "10px" }}
+                    />
+                  </label>
+                </div>
+              </div>
+
+                <Button
+                variant = "raised"
+                type="submit"
+                fullWidth
+                color="lightBlue"
+                style={{ fontWeight: "bold" }}
+                id="Btn"
+              >
+                الدخول
+              </Button>
+
+            </form>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
 }
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import Login from "./components/login/Login";
-import SecHome from "./components/homepage-sec/page-sec";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<SecHome />, document.getElementById("root"));
+import './index.css';
+import App from './App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render((
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+), document.getElementById('root'));
